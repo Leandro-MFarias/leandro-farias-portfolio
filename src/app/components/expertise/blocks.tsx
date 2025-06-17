48777ca4a08d498afaecb345b87e55636b042d50
Leandro Farias <leandrorf1606@gmail.com>
2025-06-17 01:36:45 -0300

    style: width of container on expertise

--- src/app/components/expertise/blocks.tsx
+++ src/app/components/expertise/blocks.tsx
@@ -9,14 +9,14 @@ interface BlocksProps {
 
 export function Blocks({ icon, title, description, span }: BlocksProps) {
   return (
-    <div className="shadow-hero flex md:max-w-[420px] space-x-2 rounded-2xl p-4 py-6 md:py-4">
+    <div className="shadow-hero flex md:max-w-[480px] space-x-2 rounded-2xl p-6">
       {icon}
       <div className="flex flex-col space-y-3">
         <div className="flex-1 space-y-1">
-          <h4 className="text-xl font-bold">{title}</h4>
-          <p className="text-muted-foreground text-sm">{description}</p>
+          <h4 className="text-xl md:text-2xl font-semibold">{title}</h4>
+          <p className="text-muted-foreground text-sm md:text-base">{description}</p>
         </div>
-        <span className="text-muted-foreground/70 text-sm">{span}</span>
+        <span className="text-muted-foreground/60 text-sm font-semibold">{span}</span>
       </div>
     </div>
   );
